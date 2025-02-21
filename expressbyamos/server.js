6833444493ffb3c30b12e6acdf11b8cdd22f1f5a
Amos Clever Seiyefa <cleverdraw5000@gmail.com>
2025-02-21 19:58:45 +0100

    initial commit Add index.html, index.js, inf307codes, inf307codes.pub, package-lock.json, package.json, routes/index.js, server.js, views/index.ejs, views/layouts/layout.ejs

---
+++ expressbyamos/server.js
@@ -0,0 +1,28 @@
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
+}
+
+const express = require('express');
+const expressLayouts = require('express-ejs-layouts');
+const app = express();
+
+const indexRouter = require('./routes/index')
+
+const mongoose = require('mongoose');
+
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views');
+app.set('layout', 'layouts/layout');
+app.use(expressLayouts);
+app.use(express.static('public'));
+
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true });
+const db = mongoose.connection;
+db.on('error', error => console.error(error));
+db.once('open', () => console.log('Connected to MongoDB'));
+
+app.use('/', indexRouter)
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log('Server is running on port 3000');
+});
